679c219635cb9175fed126d6c59439eaac418e17
shinichi yamaguchi <shinichi62@gmail.com>
2015-05-20 00:17:40 +0900

    Add font setting

--- inits/00_init.el
+++ inits/00_init.el
@@ -23,6 +23,24 @@
 (global-set-key (kbd "M-g") 'goto-line)
 (define-key global-map (kbd "\C-cl") 'toggle-truncate-lines)
 
+;; Show tab, zenkaku-space, white spaces at end of line
+;; http://www.bookshelf.jp/soft/meadow_26.html#SEC317
+(defface my-face-tab         '((t (:foreground "Yellow" :underline t))) nil :group 'my-faces)
+(defface my-face-zenkaku-spc '((t (:background "LightBlue"))) nil :group 'my-faces)
+(defface my-face-spc-at-eol  '((t (:foreground "Red" :underline t))) nil :group 'my-faces)
+(defvar my-face-tab         'my-face-tab)
+(defvar my-face-zenkaku-spc 'my-face-zenkaku-spc)
+(defvar my-face-spc-at-eol  'my-face-spc-at-eol)
+(defadvice font-lock-mode (before my-font-lock-mode ())
+  (font-lock-add-keywords
+   major-mode
+   '(("\t" 0 my-face-tab append)
+     ("　" 0 my-face-zenkaku-spc append)
+     ("[ \t]+$" 0 my-face-spc-at-eol append)
+     )))
+(ad-enable-advice 'font-lock-mode 'before 'my-font-lock-mode)
+(ad-activate 'font-lock-mode)
+
 ; Auto-complete
 (require 'auto-complete-config)
 (add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
--- inits/windows-00_init.el
+++ inits/windows-00_init.el
@@ -2,15 +2,17 @@
 ;;; Commentary:
 ;;; Code:
 
+; Window
+(if window-system
+  (progn
+    (setq initial-frame-alist '((width . 130) (height . 45) (top . 20) (left . 60)))
+    ))
+
+; Menu
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+
 ; Path
-;(setq exec-path (append exec-path '("C:\\cygwin64\\bin")))
-;(setenv "PATH" "C:\\Program Files (x86)\\Git\\bin")
-;(setq exec-path (append exec-path '("C:\\Program Files (x86)\\Git\\bin")))
-;(setenv "PATH"
-;  (concat
-;  "C:\\cygwin64\\bin" ";"
-;  (getenv "PATH")
-;  ))
 (setenv "PATH" "C:\\cygwin64\\bin")
 (setq grep-find-command "find . -type d -path '*CVS*' -prune -o -print0 |xargs -0 grep -niH ")
 
