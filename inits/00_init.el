f045b4e4e36a21080846f86d88e4e31dd7c1e060
shinichi yamaguchi <shinichi62@gmail.com>
2015-06-12 23:57:00 +0900

    Modify font setting

--- inits/00_init.el
+++ inits/00_init.el
@@ -8,7 +8,6 @@
 ; Encoding
 (set-language-environment "Japanese")
 (prefer-coding-system 'utf-8-unix)
-;(prefer-coding-system 'shift_jis)
 (set-buffer-file-coding-system 'utf-8)
 (set-terminal-coding-system 'utf-8)
 (set-keyboard-coding-system 'utf-8)
--- inits/windows-00_init.el
+++ inits/windows-00_init.el
@@ -12,6 +12,12 @@
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 
+
+; Font
+(set-face-attribute 'default nil :family "Consolas" :height 130)
+(set-fontset-font nil 'japanese-jisx0208 (font-spec :family "MeiryoKe_Console"))
+(setq face-font-rescale-alist '(("MeiryoKe_Console" . 1.08)))
+
 ; Path
 (setenv "PATH" "C:\\cygwin64\\bin")
 (setq grep-find-command "find . -type d -path '*CVS*' -prune -o -print0 |xargs -0 grep -niH ")
