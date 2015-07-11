eb92026e6c5f4fa65d00b0ecde2fa2b495fc2433
shinichi yamaguchi <shinichi62@gmail.com>
2015-07-11 14:34:05 +0900

    change helm color

--- inits/00_init.el
+++ inits/00_init.el
@@ -56,10 +56,6 @@
 ; Helm
 (require 'helm-config)
 (helm-mode 1)
-; For find-file etc
-(define-key helm-find-files-map (kbd "TAB") 'helm-execute-persistent-action)
-; For helm-find-files etc
-(define-key helm-read-file-map (kbd "TAB") 'helm-execute-persistent-action)
 ; define key
 (define-key global-map (kbd "M-x")     'helm-M-x)
 (define-key global-map (kbd "C-x C-f") 'helm-find-files)
@@ -67,9 +63,13 @@
 (define-key global-map (kbd "M-y")     'helm-show-kill-ring)
 (define-key global-map (kbd "C-c i")   'helm-imenu)
 (define-key global-map (kbd "C-x b")   'helm-buffers-list)
-
+(define-key helm-find-files-map (kbd "TAB") 'helm-execute-persistent-action)
+(define-key helm-read-file-map (kbd "TAB") 'helm-execute-persistent-action)
 (define-key helm-map (kbd "C-h") 'delete-backward-char)
 (define-key helm-find-files-map (kbd "C-h") 'delete-backward-char)
+; color
+(custom-set-faces
+ '(helm-selection ((t (:background "magenta" :distant-foreground "black")))))
 
 ; Linum
 (require 'linum)
